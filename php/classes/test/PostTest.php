4873a426ac5e2a449c7b1835cc0623039a216654
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-27 16:13:16 -0700

    post test: testGetValidPostsByPostDateRange()

--- php/classes/test/PostTest.php
+++ php/classes/test/PostTest.php
@@ -253,6 +253,27 @@ class PostTest extends PotentialBroccoliTest {
 	/**
 	 * test grabbing Posts by post date range
 	 **/
+	public function testGetValidPostsByPostDateRange() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("post");
+
+		//create a new post and insert
+		$post = new Post(null, $this->profile->getProfileId(), $this->VALID_CONTENT, $this->VALID_DATE, $this->VALID_TITLE);
+		$post->insert($this->getPDO());
+
+		//grab the posts from mysql, verify row count and namespace is correct
+		$results = Post::getPostsByPostDateRange($this->getPDO(), $this->SUNRISE_DATE, $this->SUNSET_DATE);
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\PotentialBroccoli\\Post", $results);
+
+		//verify that all fields match
+		$pdoPost = $results[0];
+		$this->assertEquals($pdoPost->getPostProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoPost->getPostContent(), $this->VALID_CONTENT);
+		$this->assertEquals($pdoPost->getPostDate(), $this->VALID_DATE);
+		$this->assertEquals($pdoPost->getPostTitle(), $this->VALID_TITLE);
+	}
 
 	/**
 	 * test grabbing Posts by a date that does not exist
