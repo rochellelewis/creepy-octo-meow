223c8039e6749fa339de47a1331b32dce01d4d2b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-27 00:17:12 -0700

    profile test: testGetValidProfileByProfileActivationToken()

--- php/classes/test/ProfileTest.php
+++ php/classes/test/ProfileTest.php
@@ -178,6 +178,23 @@ class ProfileTest extends PotentialBroccoliTest {
 	/**
 	 * test grabbing a Profile by profile activation token
 	 **/
+	public function testGetValidProfileByProfileActivationToken() {
+		//count number of rows and save for later
+		$numRows = $this->getConnection()->getRowCount("profile");
+
+		//create new profile and insert
+		$profile = new Profile(null, $this->VALID_ACTIVATION, $this->VALID_EMAIL, $this->VALID_HASH, $this->VALID_SALT, $this->VALID_USERNAME);
+		$profile->insert($this->getPDO());
+
+		//grab profile back from mysql and check that all fields match
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($pdoProfile->getProfileActivationToken(), $this->VALID_ACTIVATION);
+		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_EMAIL);
+		$this->assertEquals($pdoProfile->getProfileHash(), $this->VALID_HASH);
+		$this->assertEquals($pdoProfile->getProfileSalt(), $this->VALID_SALT);
+		$this->assertEquals($pdoProfile->getProfileUsername(), $this->VALID_USERNAME);
+	}
 
 	/**
 	 * test grabbing a Profile by an activation token that does not exist
