66cddb8992a98aef2eb7cf330fbb744f76fa417d
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-29 01:03:40 -0700

    add base files for apis that need to be created...

---
+++ public_html/apis/signin/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once (dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\PotentialBroccoli\Profile;
+
+/**
+ * API for app sign in, Profile class
+ *
+ * POST requests are supported.
+ *
+ * @author Rochelle Lewis <rlewis37@cnm.edu>
+ **/
+
+//check the session status. If it is not active, start the session.
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
