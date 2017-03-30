03bc0f61676f573569a165bc438957282215584f
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-29 21:13:07 -0700

    update phpunit base class with new codebase

--- php/classes/test/PotentialBroccoliTest.php
+++ php/classes/test/PotentialBroccoliTest.php
@@ -1,8 +1,16 @@
 <?php
-namespace Edu\Cnm\PotentialBroccoli\Test;
+namespace Edu\Cnm\DataDesign\Test;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
+use PHPUnit\DbUnit\DataSet\QueryDataSet;
+use PHPUnit\DbUnit\Database\Connection;
+use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
 
 // grab the encrypted properties file
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 
 /**
  * Abstract class containing universal and project specific mySQL parameters
@@ -11,18 +19,19 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
  * parameters about the project so that table specific tests can share the parameters in on place. To use it:
  *
  * 1. Rename the class from DataDesignTest to a project specific name (e.g., ProjectNameTest)
- * 2. Put the class in the correct namespace (e.g., Edu\Cnm\ProjectName\Test)
- * 3. Modify DataDesignTest::getDataSet() to include all the tables in your project.
- * 4. Modify DataDesignTest::getConnection() to include the correct mySQL properties file.
- * 5. Have all table specific tests include this class.
+ * 2. Modify DataDesignTest::getDataSet() to include all the tables in your project.
+ * 3. Modify DataDesignTest::getConnection() to include the correct mySQL properties file.
+ * 4. Have all table specific tests include this class.
  *
  * *NOTE*: Tables must be added in the order they were created in step (2).
  *
  * @author Dylan McDonald <dmcdonald21@cnm.edu>
  **/
-abstract class PotentialBroccoliTest extends \PHPUnit_Extensions_Database_TestCase {
+abstract class PotentialBroccoliTest extends TestCase {
+	use TestCaseTrait;
+
 	/**
-	 * invalid id to use for an INT UNSIGNED field (maximum allowed INT UNSIGNED in mySQL) + 1
+	 * invalid id to use for an INT UNSIGNED field (maximum allowed INT UNSIGTNED in mySQL) + 1
 	 * @see https://dev.mysql.com/doc/refman/5.6/en/integer-types.html mySQL Integer Types
 	 * @var int INVALID_KEY
 	 **/
@@ -30,17 +39,17 @@ abstract class PotentialBroccoliTest extends \PHPUnit_Extensions_Database_TestCa
 
 	/**
 	 * PHPUnit database connection interface
-	 * @var \PHPUnit_Extensions_Database_DB_IDatabaseConnection $connection
+	 * @var Connection $connection
 	 **/
 	protected $connection = null;
 
 	/**
 	 * assembles the table from the schema and provides it to PHPUnit
 	 *
-	 * @return \PHPUnit_Extensions_Database_DataSet_QueryDataSet assembled schema for PHPUnit
+	 * @return QueryDataSet assembled schema for PHPUnit
 	 **/
 	public final function getDataSet() {
-		$dataset = new \PHPUnit_Extensions_Database_DataSet_QueryDataSet($this->getConnection());
+		$dataset = new QueryDataSet($this->getConnection());
 
 		// add all the tables for the project here
 		// THESE TABLES *MUST* BE LISTED IN THE SAME ORDER THEY WERE CREATED!!!!
@@ -54,29 +63,29 @@ abstract class PotentialBroccoliTest extends \PHPUnit_Extensions_Database_TestCa
 	 *
 	 * @see https://phpunit.de/manual/current/en/fixtures.html#fixtures.more-setup-than-teardown PHPUnit Fixtures: setUp and tearDown
 	 * @see https://github.com/sebastianbergmann/dbunit/issues/37 TRUNCATE fails on tables which have foreign key constraints
-	 * @return \PHPUnit_Extensions_Database_Operation_Composite array containing delete and insert commands
+	 * @return Composite array containing delete and insert commands
 	 **/
 	public final function getSetUpOperation() {
-		return new \PHPUnit_Extensions_Database_Operation_Composite(array(
-			\PHPUnit_Extensions_Database_Operation_Factory::DELETE_ALL(),
-			\PHPUnit_Extensions_Database_Operation_Factory::INSERT()
-		));
+		return new Composite([
+			Factory::DELETE_ALL(),
+			Factory::INSERT()
+		]);
 	}
 
 	/**
 	 * templates the tearDown method that runs after each test; this method expunges the database after each run
 	 *
-	 * @return \PHPUnit_Extensions_Database_Operation_IDatabaseOperation delete command for the database
+	 * @return Operation delete command for the database
 	 **/
 	public final function getTearDownOperation() {
-		return(\PHPUnit_Extensions_Database_Operation_Factory::DELETE_ALL());
+		return(Factory::DELETE_ALL());
 	}
 
 	/**
 	 * sets up the database connection and provides it to PHPUnit
 	 *
 	 * @see <https://phpunit.de/manual/current/en/database.html#database.configuration-of-a-phpunit-database-testcase>
-	 * @return \PHPUnit_Extensions_Database_DB_IDatabaseConnection PHPUnit database connection interface
+	 * @return Connection PHPUnit database connection interface
 	 **/
 	public final function getConnection() {
 		// if the connection hasn't been established, create it
