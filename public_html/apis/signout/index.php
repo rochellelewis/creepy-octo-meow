dc75e1bd848e02e7b7c7e6c8ff58dc3d0f289095
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-30 03:38:21 -0700

    add signout api

--- public_html/apis/signout/index.php
+++ public_html/apis/signout/index.php
@@ -19,3 +19,55 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
+/**
+ * Prepare an empty reply.
+ *
+ * Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+ *
+ * We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
+ **/
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+
+	//grab the database connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/rlewis37.ini");
+
+	//determine which HTTP method, store the result in $method
+	$method = array_key_exists("HTTP_x_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	if($method === "GET") {
+
+		//if the session isn't active, they are already logged out
+		if(session_status() !== PHP_SESSION_ACTIVE) {
+			$reply->message = "You've already signed out.";
+		}
+
+		//empty out the session
+		$_SESSION = [];
+
+		//update reply upon successful sign out
+		$reply->message = "Goodbye!";
+
+	} else {
+		throw (new \InvalidArgumentException("Invalid HTTP request!"));
+	}
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+//sets up the response header.
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+//finally - JSON encode the $reply object and echo it back to the front end.
+echo json_encode($reply);
