e31da57cc00b21a3de3682cc53fbdfb82884f9ac
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-30 05:59:16 -0700

    begin post api.

--- public_html/apis/post/index.php
+++ public_html/apis/post/index.php
@@ -19,3 +19,46 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
+/**
+ * Prepare an empty reply.
+ *
+ * Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+ *
+ * We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
+ **/
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+
+	//grab the database connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/rlewis37.ini");
+
+	//determine which HTTP method, store the result in $method
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//throw exception if not logged in
+
+	//sanitize and store input
+
+	//begin if blocks for the various HTTP requests
+
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+//sets up the response header.
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+//finally - JSON encode the $reply object and echo it back to the front end.
+echo json_encode($reply);
