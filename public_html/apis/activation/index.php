d78b2444710f1a25836b5bbf94ea0ec9cb13383c
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-03-30 22:10:05 -0700

    create activation api

--- public_html/apis/activation/index.php
+++ public_html/apis/activation/index.php
@@ -19,3 +19,75 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
 
+/**
+ * Prepare an empty reply.
+ *
+ * Here we create a new stdClass named $reply. A stdClass is basically an empty bucket that we can use to store things in.
+ *
+ * We will use this object named $reply to store the results of the call to our API. The status 200 line adds a state variable to $reply called status and initializes it with the integer 200 (success code). The proceeding line adds a state variable to $reply called data. This is where the result of the API call will be stored. We will also update $reply->message as we proceed through the API.
+ **/
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+
+	//grab the database connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/rlewis37.ini");
+
+	//determine which HTTP method, store the result in $method
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize and store input (activation token)
+	$profileActivationToken = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING);
+
+	//check that activation token is a valid hash
+	if(ctype_xdigit($profileActivationToken) === false) {
+		throw (new \InvalidArgumentException("Activation token is invalid.", 405));
+	}
+
+	//check that activation token is correct length
+	if(strlen($profileActivationToken) !== 32) {
+		throw (new \InvalidArgumentException("Activation Token is invalid length.", 405));
+	}
+
+	if($method === "GET") {
+
+		setXsrfCookie();
+
+		//grab the profile by activation token
+		$profile = Profile::getProfileByProfileActivationToken($pdo, $profileActivationToken);
+
+		if(empty($profile) === true) {
+			throw (new \InvalidArgumentException("No profile found for the activation token.", 404));
+		}
+
+		//set the activation token to null
+		$profile->setProfileActivationToken(null);
+
+		//update profile
+		$profile->update($pdo);
+
+		//update reply
+		$reply->message = "Profile activated!";
+
+	} else {
+		throw (new \InvalidArgumentException("Invalid HTTP request!", 405));
+	}
+
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
+}
+
+//sets up the response header.
+header("Content-type: application/json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+
+//finally - JSON encode the $reply object and echo it back to the front end.
+echo json_encode($reply);
