234c561c5a8aed7d807703176af6eded7b7d03d4
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-01 00:20:11 -0700

    remove mailgun - we're using swiftmailer now

--- public_html/apis/signup/index.php
+++ public_html/apis/signup/index.php
@@ -2,7 +2,6 @@
 require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
 require_once (dirname(__DIR__, 3) . "/php/classes/autoload.php");
 require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
-//require_once (dirname(__DIR__, 3) . "/php/lib/mailgun.php");
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\CreepyOctoMeow\Profile;
@@ -96,9 +95,6 @@ try {
 <h2>One more step to activate your account.</h2>
 <p>Visit the following link to complete the sign-up process: <a href="$confirmLink">$confirmLink<a></p>
 EOF;
-
-		//mailgun it!
-		//$response = mailgunHandler($senderName, $senderEmail, $recipientName, $recipientEmail, $subject, $message);
 
 		//swiftmail it!
 		$swiftMessage = Swift_Message::newInstance();
