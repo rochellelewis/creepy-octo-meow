0cd0a67e5d16c2641aa998e2f0e4be09f9b3dea0
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-01 03:19:42 -0700

    static front end: delete post modal

--- public_html/static/feed.php
+++ public_html/static/feed.php
@@ -28,7 +28,7 @@
 						<div class="panel panel-info">
 							<div class="panel-heading">
 								<h3 class="panel-title pull-left">Post Title</h3>
-								<button class="pull-right btn btn-default btn-sm"><i class="fa fa-trash-o"></i></button>
+								<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#delete-post-modal"><i class="fa fa-trash-o"></i></button>
 								<button class="pull-right btn btn-default btn-sm"><i class="fa fa-pencil"></i></button>
 								<div class="clearfix"></div>
 							</div>
@@ -50,6 +50,7 @@
 		<?php require_once ("modals/new-post-modal.php");?>
 
 		<!-- delete modal -->
+		<?php require_once ("modals/delete-post-modal.php");?>
 
 		<!-- edit modal -->
 
