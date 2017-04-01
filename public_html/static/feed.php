9470c487bd37fc012d6a40b9f98b477326f05be1
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-01 01:11:55 -0700

    begin static front end sandbox

---
+++ public_html/static/feed.php
@@ -0,0 +1,17 @@
+<!-- insert head tag -->
+<?php require_once ("head-utils.php");?>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- insert header -->
+		<?php require_once ("header.php");?>
+
+		<main>
+			<h2>main content</h2>
+		</main>
+	</div><!--./sfooter-content-->
+
+	<!--insert footer -->
+	<?php require_once ("footer.php");?>
+</body>
+</html>
---
+++ public_html/static/header.php
@@ -0,0 +1,3 @@
+<header>
+	<h1>header</h1>
+</header>
