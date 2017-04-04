4904c35361d947f4f282b08a29576cc19e021534
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-03 22:18:24 -0700

    angular install: base /app files

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
+	selector: 'creepy-octo-meow',
+
+	// templateUrl path to your public_html/templates directory.
+	templateUrl: './templates/creepy-octo-meow.php'
+})
+
+export class AppComponent {}
