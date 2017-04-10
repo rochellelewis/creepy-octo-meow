d3c036b4a823c1a242baaf15f9154ab664e948a2
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 15:30:11 -0600

    add profile view template, component, route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,12 +1,14 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {FeedComponent} from "./components/feed-component";
+import {ProfileComponent} from "./components/profile-component";
 
-export const allAppComponents = [HomeComponent, FeedComponent];
+export const allAppComponents = [HomeComponent, FeedComponent, ProfileComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
-	{path: "feed", component: FeedComponent}
+	{path: "feed", component: FeedComponent},
+	{path: "profile", component: ProfileComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
