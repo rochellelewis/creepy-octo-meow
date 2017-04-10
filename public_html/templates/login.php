bc36febc30b31b01951557e6c755d846e56da4c8
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:12:01 -0600

    add login html to template

--- public_html/templates/login.php
+++ public_html/templates/login.php
@@ -1,1 +1,31 @@
-login
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="login">Log in, Boo.</h4>
+			</div>
+			<div class="modal-body">
+				<form action="" class="form-horizontal">
+					<div class="form-group">
+						<label for="profileEmail" class="col-sm-2 control-label">Email</label>
+						<div class="col-sm-10">
+							<input type="email" class="form-control" id="profileEmail" name="profileEmail" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="profilePassword" class="col-sm-2 control-label">Password</label>
+						<div class="col-sm-10">
+							<input type="password" class="form-control" id="profilePassword" name="profilePassword" placeholder="Password">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-10">
+							<button type="submit" class="btn btn-default">Log in</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
