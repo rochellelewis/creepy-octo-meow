1198acf2ef68684f64612de915c6a2c0839f0cd6
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:04:56 -0600

    add navbar html to template

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -1,1 +1,42 @@
-navbar template
+<header>
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<div class="container-fluid">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" routerLink="">=^. .^=</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#new-post-modal"><i class="fa fa-plus"></i> Post</button></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hai, <em>username</em>&nbsp;<span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a routerLink="/profile">Profile</a></li>
+							<li><a routerLink="">Log Out</a></li>
+						</ul>
+					</li>
+					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> Log In</button></li>
+					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#signup-modal"><i class="fa fa-paw"></i> Sign Up</button></li>
+				</ul>
+				<form class="navbar-form navbar-right">
+					<div class="form-group">
+						<div class="input-group">
+							<input type="text" class="form-control" id="search-feed" name="search-feed" placeholder="search posts">
+							<span class="input-group-btn">
+										<button class="btn btn-default"><i class="fa fa-search"></i></button>
+									</span>
+						</div>
+					</div>
+				</form>
+			</div><!-- /.navbar-collapse -->
+		</div><!-- /.container-fluid -->
+	</nav>
+</header>
