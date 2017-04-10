c062fa899846a97ccf26d3f499df1de98d01dec4
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:07:11 -0600

    add home html to template

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,5 +1,24 @@
-<section class="welcome">
-	<div class="container">
-		<h1>Creepy Octo Meow</h1>
-	</div>
-</section>
+<navbar></navbar>
+
+<main>
+	<div class="container-fluid">
+
+		<!-- title row -->
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Creepy Octo Meow.</h1>
+			</div>
+		</div>
+
+		<!-- content row -->
+		<div class="row">
+			<div class="col-sm-6">
+				<p>This is some content</p>
+			</div>
+			<div class="col-sm-6">
+				<p>This is some more content.</p>
+			</div>
+		</div>
+
+	</div><!--./container-fluid-->
+</main>
