c7a3fd41fa5cce87c82858e695b96a014200785b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 15:36:11 -0600

    add navbar view template, component, route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,13 +2,15 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {FeedComponent} from "./components/feed-component";
 import {ProfileComponent} from "./components/profile-component";
+import {NavbarComponent} from "./components/navbar-component";
 
-export const allAppComponents = [HomeComponent, FeedComponent, ProfileComponent];
+export const allAppComponents = [HomeComponent, FeedComponent, ProfileComponent, NavbarComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "feed", component: FeedComponent},
-	{path: "profile", component: ProfileComponent}
+	{path: "profile/:id", component: ProfileComponent},
+	{path: "**", redirectTo: ""}
 ];
 
 export const appRoutingProviders: any[] = [];
---
+++ app/components/navbar-component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+
+@Component({
+	templateUrl: "./templates/navbar.php",
+	selector: "navbar"
+})
+
+export class NavbarComponent { }
