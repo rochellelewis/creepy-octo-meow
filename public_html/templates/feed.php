9b8b15bf259ca66b6a7d4066a8551e6c0d064fde
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:08:46 -0600

    add feed html to template

--- public_html/templates/feed.php
+++ public_html/templates/feed.php
@@ -1,1 +1,38 @@
-feed view
+<navbar></navbar>
+
+<main>
+	<div class="container-fluid">
+
+		<!-- title row -->
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Feed Teh Kitty.</h1>
+			</div>
+		</div>
+
+		<!-- content row -->
+		<div class="row">
+			<div class="col-xs-12">
+
+				<!-- single post item -->
+				<div class="panel panel-info">
+					<div class="panel-heading">
+						<h3 class="panel-title pull-left">Post Title</h3>
+						<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#delete-post-modal"><i class="fa fa-trash-o"></i></button>
+						<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#update-post-modal"><i class="fa fa-pencil"></i></button>
+						<div class="clearfix"></div>
+					</div>
+					<div class="panel-body">
+						<div class="post-date">
+							<small><em>author</em> | <em>date</em></small>
+						</div>
+						<div class="post-content">
+							Post content
+						</div>
+					</div>
+				</div><!--/.panel-->
+
+			</div><!--./col-md-8-->
+		</div><!--/.row-->
+	</div><!--/.container-fluid-->
+</main>
