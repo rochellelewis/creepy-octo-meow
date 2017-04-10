bd84e0b2340dbfd5f32f13d9c261599c1ccac789
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:13:12 -0600

    add delete post html to template

--- public_html/templates/delete-post.php
+++ public_html/templates/delete-post.php
@@ -1,1 +1,16 @@
-delete post
+<div class="modal fade" id="delete-post-modal" tabindex="-1" role="dialog" aria-labelledby="delete-post">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="delete-post">Are U sure?</h4>
+			</div>
+			<div class="modal-body">
+				<p>Are you sure you want to permanently delete the following post?</p>
+				<p>{{post title}}</p>
+				<button class="btn btn-danger">Delete it!</button>
+				<button class="btn btn-default" data-dismiss="modal" type="button">Cancel</button>
+			</div>
+		</div>
+	</div>
+</div>
