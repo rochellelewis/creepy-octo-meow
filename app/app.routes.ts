f2710ccd9249c0457d3fa43605afa31d81f0045c
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 15:20:36 -0600

    add feed view template, component, route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {FeedComponent} from "./components/feed-component";
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, FeedComponent];
 
 export const routes: Routes = [
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "feed", component: FeedComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
