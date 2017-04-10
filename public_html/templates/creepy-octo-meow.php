99019757914a0753980d6e523b893c523ff6a03b
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:00:43 -0600

    add footer to base angular template

--- public_html/templates/creepy-octo-meow.php
+++ public_html/templates/creepy-octo-meow.php
@@ -1,3 +1,16 @@
 <div class="sfooter-content">
 	<router-outlet></router-outlet>
 </div>
+
+<footer>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6">
+				Creepy Octo Meow
+			</div>
+			<div class="col-sm-6 text-right">
+				<a href="https://github.com/rlewis2892/creepy-octo-meow" target="_blank">GitHub</a>
+			</div>
+		</div>
+	</div>
+</footer>
