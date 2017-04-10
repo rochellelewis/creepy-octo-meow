7d43fbcd459c93a4d4452f64556ee5083ffbf0b3
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2017-04-10 16:10:37 -0600

    add profile html to template

--- public_html/templates/profile.php
+++ public_html/templates/profile.php
@@ -1,1 +1,30 @@
-profile view
+<navbar></navbar>
+
+<main>
+	<div class="container-fluid">
+
+		<!-- title row -->
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>{{Username}}</h1>
+			</div>
+		</div>
+
+
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<a class="btn btn-default btn-sm" href="feed.php"><i class="fa fa-arrow-left"></i> Back to feed</a>
+						<button class="btn btn-default btn-sm" data-toggle="modal" data-target="#update-profile-modal"><i class="fa fa-pencil"></i>&nbsp;Edit Profile</button>
+						<div class="clearfix"></div>
+					</div>
+					<div class="panel-body">
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+	</div><!--./container-fluid-->
+</main>
