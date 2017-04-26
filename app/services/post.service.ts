0864f667c84aeb394ac076baa045aeee8719a223
Rochelle Lewis <rlewis37@gmail.com>
2017-04-26 14:51:46 -0600

    add Angular Post Service

--- app/services/post.service.ts
+++ app/services/post.service.ts
@@ -40,23 +40,44 @@ export class PostService extends BaseService {
 
 	// connect to post API and get post by id
 	getPost(id: number) : Observable<Post> {
-		return(this.http.get(this.postUrl, Post)
+		return(this.http.get(this.postUrl + id)
 			.map(BaseService.extractData)
 			.catch(BaseService.handleError));
 	}
 
 	// connect to post API and get posts by profile id
 	getPostsByProfileId(postProfileId : number) : Observable<Post[]> {
-		return(this.http.get()
-
-		);
+		return(this.http.get(this.postUrl + postProfileId, Post)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
 	}
 
 	// connect to post API and get posts by content
+	getPostsByPostContent(postContent : string) : Observable<Post[]> {
+		return(this.http.get(this.postUrl + postContent)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
 
 	// connect to post API and get posts by date range
+	getPostsByPostDateRange(postDate: number) :Observable<Post[]> {
+		return(this.http.get(this.postUrl + postDate)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
 
 	// connect to post API and get post by title
+	getPostsByPostTitle(postTitle: string) : Observable<Post[]> {
+		return(this.http.get(this.postUrl + postTitle)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError));
+	}
 
 	// connect to post API and get all posts
+	getAllPosts() : Observable<Post[]> {
+		return(this.http.get(this.postUrl)
+			.map(BaseService.extractData)
+			.catch(BaseService.handleError)
+		);
+	}
 }
