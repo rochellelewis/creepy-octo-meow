0f3d3861595491850c2836314f71a9fee7abbfb9
Rochelle Lewis <rlewis37@gmail.com>
2017-05-03 17:00:41 -0600

    import all services in app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,12 +5,20 @@ import {HttpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
+import {BaseService} from "./services/base.service";
+import {PostService} from "./services/post.service";
+import {ProfileService} from "./services/profile.service";
+import {SignInService} from "./services/sign-in.service";
+import {SignOutService} from "./services/sign-out.service";
+import {SignUpService} from "./services/sign-up.service";
+
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
 	imports:      [BrowserModule, FormsModule, HttpModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [appRoutingProviders]
+	providers:    [appRoutingProviders, PostService]
 })
+
 export class AppModule {}
--- app/components/feed.component.ts
+++ app/components/feed.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
-import {Post} from "../classes/post";
 import {PostService} from "../services/post.service";
+import {Post} from "../classes/post";
+import {Status} from "../classes/status";
 import {Observable} from "rxjs";
 import "rxjs/add/observable/from";
 
@@ -11,13 +12,14 @@ import "rxjs/add/observable/from";
 
 export class FeedComponent implements OnInit {
 
-	postsFiltered : Post[] = [];
-	postObservable : Observable<Post> = null;
+	posts: Post[] = [];
+	post: Post = new Post(0, 0, "", "", "");
+	status: Status = null;
 
 	constructor(private postService: PostService, private router: Router) {}
 
 	ngOnInit() : void {
-		this.postService.getAllPosts();
+		this.postService.getAllPosts().subscribe(posts => this.posts = posts);
 	}
 
 }
