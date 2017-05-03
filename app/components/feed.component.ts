1f1d8c88dc634862ce20abe003d2d82f168c6275
Rochelle Lewis <rlewis37@gmail.com>
2017-05-03 13:27:23 -0600

    update FeedComponent with OnInit, update template with binding

--- app/components/feed.component.ts
+++ app/components/feed.component.ts
@@ -1,7 +1,23 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Post} from "../classes/post";
+import {PostService} from "../services/post.service";
+import {Observable} from "rxjs";
+import "rxjs/add/observable/from";
 
 @Component({
 	templateUrl: "./templates/feed.php"
 })
 
-export class FeedComponent { }
+export class FeedComponent implements OnInit {
+
+	postsFiltered : Post[] = [];
+	postObservable : Observable<Post> = null;
+
+	constructor(private postService: PostService, private router: Router) {}
+
+	ngOnInit() : void {
+		this.postService.getAllPosts();
+	}
+
+}
--- public_html/templates/feed.php
+++ public_html/templates/feed.php
@@ -14,23 +14,24 @@
 		<div class="row">
 			<div class="col-xs-12">
 
-				<!-- single post item -->
-				<div class="panel panel-info">
+				<!-- BEGIN POST ITEM -->
+				<div class="panel panel-info" *ngFor="let post of postsFiltered">
 					<div class="panel-heading">
-						<h3 class="panel-title pull-left">Post Title</h3>
+						<h3 class="panel-title pull-left">{{post.postTitle}}</h3>
 						<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#delete-post-modal"><i class="fa fa-trash-o"></i></button>
 						<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#update-post-modal"><i class="fa fa-pencil"></i></button>
 						<div class="clearfix"></div>
 					</div>
 					<div class="panel-body">
 						<div class="post-date">
-							<small><em>author</em> | <em>date</em></small>
+							<small><em>{{post.postProfileId}}</em> | <em>{{post.postDate}}</em></small>
 						</div>
 						<div class="post-content">
-							Post content
+							{{post.postContent}}
 						</div>
 					</div>
 				</div><!--/.panel-->
+				<!-- END POST ITEM -->
 
 			</div><!--./col-md-8-->
 		</div><!--/.row-->
