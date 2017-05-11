472e6ec5d913855d5fc161a2a1fd8ea72d7bf8b6
Rochelle Lewis <rlewis37@gmail.com>
2017-05-11 15:39:39 -0600

    minor cleanup on navbar template

--- public_html/templates/navbar.php
+++ public_html/templates/navbar.php
@@ -15,27 +15,34 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">
+
+					<!-- show these buttons only if logged in -->
 					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#new-post-modal"><i class="fa fa-plus"></i> Post</button></li>
 					<li class="dropdown">
 						<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hai, <em>username</em>&nbsp;<span class="caret"></span></a>
 						<ul class="dropdown-menu">
-							<li><a routerLink="/profile/:id">Profile</a></li>
+							<li><a routerLink="/profile">Profile</a></li>
 							<li><a routerLink="">Log Out</a></li>
 						</ul>
 					</li>
+
+					<!-- show these buttons only if NOT logged in -->
 					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#signin-modal"><i class="fa fa-sign-in"></i> Log In</button></li>
 					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#signup-modal"><i class="fa fa-paw"></i> Sign Up</button></li>
+
 				</ul>
+
 				<form class="navbar-form navbar-right">
 					<div class="form-group">
 						<div class="input-group">
 							<input type="text" class="form-control" id="search-feed" name="search-feed" placeholder="search posts">
 							<span class="input-group-btn">
-										<button class="btn btn-default"><i class="fa fa-search"></i></button>
-									</span>
+								<button class="btn btn-default"><i class="fa fa-search"></i></button>
+							</span>
 						</div>
 					</div>
 				</form>
+
 			</div><!-- /.navbar-collapse -->
 		</div><!-- /.container-fluid -->
 	</nav>
