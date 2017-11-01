956073c2c21280e496a07bd203ad0e85b4a2659f
Rochelle Lewis <rlewis37@gmail.com>
2017-11-01 14:40:02 -0600

    update SQL - primary key to uuid, now using datetime(6)

--- sql/creepyoctomeow.sql
+++ sql/creepyoctomeow.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS post;
 DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
-	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
 	profileEmail VARCHAR(64) NOT NULL,
 	profileHash CHAR(128) NOT NULL,
@@ -15,10 +15,10 @@ CREATE TABLE profile (
 );
 
 CREATE TABLE post (
-	postId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	postProfileId INT UNSIGNED NOT NULL,
+	postId BINARY(16) NOT NULL,
+	postProfileId BINARY(16) NOT NULL,
 	postContent VARCHAR(2000) NOT NULL,
-	postDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	postDate DATETIME(6) NOT NULL,
 	postTitle VARCHAR(64) NOT NULL,
 	INDEX (postProfileId),
 	FOREIGN KEY (postProfileId) REFERENCES profile(profileId),
