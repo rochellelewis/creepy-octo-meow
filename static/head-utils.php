4e1c29f475f20297cdea53e05abe7f19d63d287f
Rochelle Lewis <rlewis37@gmail.com>
2017-11-09 15:23:53 -0700

    update static ui header + head-utils w/ BSv4

--- static/head-utils.php
+++ static/head-utils.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<!-- Required meta tags -->
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
 		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
@@ -14,18 +14,10 @@
 		<!-- Custom CSS Goes HERE -->
 		<link rel="stylesheet" href="style.css" type="text/css">
 
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
-		<!-- jQuery - required for Bootstrap Components -->
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
 		<title>Creepy Octo Meow</title>
 	</head>
--- static/header.php
+++ static/header.php
@@ -1,42 +1,41 @@
 <header>
-	<nav class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">=^. .^=</a>
-			</div>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#">=^. .^=</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#new-post-modal"><i class="fa fa-plus"></i> Post</button></li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hai, <em>username</em>&nbsp;<span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="profile.php">Profile</a></li>
-							<li><a href="#">Log Out</a></li>
-						</ul>
-					</li>
-					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> Log In</button></li>
-					<li><button class="btn btn-default navbar-btn" data-toggle="modal" data-target="#signup-modal"><i class="fa fa-paw"></i> Sign Up</button></li>
-				</ul>
-				<form class="navbar-form navbar-right">
-					<div class="form-group">
-						<div class="input-group">
-							<input type="text" class="form-control" id="search-feed" name="search-feed" placeholder="search posts">
-							<span class="input-group-btn">
-										<button class="btn btn-default"><i class="fa fa-search"></i></button>
-									</span>
-						</div>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item dropdown my-0 mr-sm-2">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Hai, [name]!
+					</a>
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+						<a class="dropdown-item" href="#"><i class="fa fa-paw"></i>&nbsp;Profile</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item" href="#"><i class="fa fa-sign-out"></i>&nbsp;Log Out</a>
+					</div>
+				</li>
+				<li class="nav-item">
+					<button class="btn btn-outline-info my-0 mr-sm-2"><i class="fa fa-plus"></i>&nbsp;Post</button>
+				</li>
+				<li class="nav-item">
+					<button class="btn btn-outline-info my-0 mr-sm-2"><i class="fa fa-sign-in"></i>&nbsp;Log In</button>
+				</li>
+				<li class="nav-item">
+					<button class="btn btn-outline-info"><i class="fa fa-paw"></i>&nbsp;Sign Up!</button>
+				</li>
+
+				<form class="form-inline mb-auto mx-auto ml-sm-2">
+					<div class="input-group">
+						<input class="form-control" type="search" placeholder="Search" aria-label="Search">
+						<span class="input-group-btn">
+							<button class="btn btn-info"><i class="fa fa-search"></i></button>
+						</span>
 					</div>
 				</form>
-			</div><!-- /.navbar-collapse -->
-		</div><!-- /.container-fluid -->
+			</ul>
+		</div>
 	</nav>
 </header>
