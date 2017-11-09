bb12555c3ef4e6010ec30e6a66eff0ceb627e432
Rochelle Lewis <rlewis37@gmail.com>
2017-11-09 16:07:45 -0700

    update static ui feed layout w/ BSv4

--- static/feed.php
+++ static/feed.php
@@ -11,32 +11,32 @@
 
 				<!-- title row -->
 				<div class="row">
-					<div class="col-xs-12">
-						<h1>Feed Teh Kitty.</h1>
+					<div class="col">
+						<h1 class="mb-5">Feed Teh Kitty.</h1>
 					</div>
 				</div>
 
 				<!-- content row -->
 				<div class="row">
-					<div class="col-xs-12">
+					<div class="col">
 
 						<!-- single post item -->
-						<div class="panel panel-info">
-							<div class="panel-heading">
-								<h3 class="panel-title pull-left">Post Title</h3>
-								<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#delete-post-modal"><i class="fa fa-trash-o"></i></button>
-								<button class="pull-right btn btn-default btn-sm" data-toggle="modal" data-target="#update-post-modal"><i class="fa fa-pencil"></i></button>
-								<div class="clearfix"></div>
+						<div class="card">
+							<div class="card-header d-flex justify-content-end">
+								<h3 class="panel-title d-inline-block my-0 mr-auto">Post Title</h3>
+								<button class="btn btn-default btn-sm mr-2" data-toggle="modal" data-target="#delete-post-modal"><i class="fa fa-trash-o"></i></button>
+								<button class="btn btn-default btn-sm" data-toggle="modal" data-target="#update-post-modal"><i class="fa fa-pencil"></i></button>
 							</div>
-							<div class="panel-body">
+							<div class="card-body">
 								<div class="post-date">
 									<small><em>author</em> | <em>date</em></small>
+									<hr>
 								</div>
 								<div class="post-content">
 									Post content
 								</div>
 							</div>
-						</div><!--/.panel-->
+						</div>
 
 					</div><!--./col-md-8-->
 				</div><!--/.row-->
