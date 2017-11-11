bd00ba995c9a6b474f1706d064e16ab5e86d1a27
Rochelle Lewis <rlewis37@gmail.com>
2017-11-11 01:38:23 -0700

    webpack: update base template

--- webpack/index.php
+++ webpack/index.php
@@ -7,9 +7,21 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<base href="<?php echo dirname($_SERVER["PHP_SELF"]) . "/"; ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<script>
+			(function() {
+				var basePath = "/";
+				var baseIndex = location.pathname.split("/").findIndex(function(directory) {
+					return(directory === "public_html");
+				});
+				if(baseIndex >= 0) {
+					basePath = location.pathname.split("/").splice(0, baseIndex + 1).join("/") + "/";
+				}
+				window["_base_href"] = basePath;
+				document.write("<base href=\"" + basePath + "\" />");
+			})();
+		</script>
 
 		<title>Creepy Octo Meow</title>
 	</head>
