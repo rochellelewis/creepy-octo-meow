c7e332672bd3c04fcbd5c8d8165933ea864c8a5f
Rochelle Lewis <rlewis37@gmail.com>
2017-11-11 15:19:16 -0700

    app: update app.module.ts. Use ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,29 @@
 import {NgModule} from "@angular/core";
+import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
+//ReactiveFormsModule vs FormsModule see: https://next.angular.io/guide/reactive-forms
+import {ReactiveFormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
+//services
+/*
 import {PostService} from "./services/post.service";
 import {ProfileService} from "./services/profile.service";
 import {SignInService} from "./services/sign-in.service";
 import {SignOutService} from "./services/sign-out.service";
 import {SignUpService} from "./services/sign-up.service";
+*/
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, FormsModule, HttpModule, routing],
+	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
-	providers:    [
-		appRoutingProviders,
-		PostService,
-		ProfileService,
-		SignInService,
-		SignOutService,
-		SignUpService
-	]
+
+	//update services in providers array below:
+	providers:    [...appRoutingProviders]
 })
 
 export class AppModule {}
