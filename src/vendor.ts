35f9e7f11b19eb15eb2c788a1e3b20fb700d6758
Rochelle Lewis <rlewis37@gmail.com>
2017-11-11 15:04:14 -0700

    ng5: update vendor.ts

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,9 +1,8 @@
-// Angular 4
+// Angular 5
 import "@angular/platform-browser";
 import "@angular/platform-browser-dynamic";
 import "@angular/core";
 import "@angular/common";
-import "@angular/http";
 import "@angular/router";
 
 // RxJS
