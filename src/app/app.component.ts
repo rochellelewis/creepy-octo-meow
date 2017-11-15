4ce67b92d15131e8d586698ee5359a149d95475d
Rochelle Lewis <rlewis37@gmail.com>
2017-11-15 16:19:36 -0700

    meaningless changes to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import {Component, ViewChild} from "@angular/core";
-
-/*import {SignInComponent} from "./components/sign-in.component";
-import {SignInService} from "./services/sign-in.service";*/
+// import {SessionService} from "./services/session.service";
+// import {SignInService} from "./services/sign-in.service";
+// import {SignInComponent} from "./components/sign-in.component";
 
 @Component({
 	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
 	selector: 'creepy-octo-meow',
 
-	// templateUrl path to your public_html/templates directory.
-	// Use .html files only.
+	// templateUrl path to your public_html/templates directory. .html files only.
 	templateUrl: './templates/creepy-octo-meow.html'
 })
 
