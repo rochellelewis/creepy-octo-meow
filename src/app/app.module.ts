20c8a2df61b56d11bbaba240086279bd110dc3dc
Rochelle Lewis <rlewis37@gmail.com>
2017-11-16 11:22:33 -0700

    remove HttpResponse from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from "@angular/core";
-import {HttpClientModule, HttpResponse} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
@@ -10,7 +10,7 @@ import {SessionService} from "./services/session.service";
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, HttpResponse, ReactiveFormsModule, routing],
+	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
