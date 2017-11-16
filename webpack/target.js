5edad91f7fa3fbf63635ae4a7f96a92637f7594b
Rochelle Lewis <rlewis37@gmail.com>
2017-11-15 17:06:22 -0700

    added webpack/target.js for dev-server

---
+++ webpack/target.js
@@ -0,0 +1,4 @@
+module.exports = function() {
+	//path to your deployment API directory
+	return basepath = "https://bootcamp-coders.cnm.edu/~rlewis37/creepy-octo-meow/public_html/"; //Your URL here
+};
