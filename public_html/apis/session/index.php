f97c2ac296e0a0c60db5fe02f72a08dbcec28f23
Rochelle Lewis <rlewis37@gmail.com>
2017-11-17 21:49:33 -0700

    session api: update to check only for head request

--- public_html/apis/session/index.php
+++ public_html/apis/session/index.php
@@ -1,15 +1,14 @@
 <?php
 require_once dirname(__DIR__,3 ) .  "/php/lib/xsrf.php";
-$reply = new stdClass();
-$reply->status = 200;
-$reply->data = null;
 
 try {
+
 	//verify the HTTP method being used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	// if the HTTP method is head check/start the PHP session and set the XSRF token
-	if($method === "GET" || $method === "HEAD") {
+	if($method === "HEAD") {
+
 		//verify the session, start if not active
 		if(session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
@@ -20,9 +19,7 @@ try {
 		throw (new \InvalidArgumentException("Attempting to brew coffee with a teapot", 418));
 	}
 
-} catch(\Exception  | \TypeError $exception) {
+} catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
 }
-
-echo json_encode($reply);
