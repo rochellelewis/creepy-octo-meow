a8ecde27ea17f847405f8524800d02a977d50079
Rochelle Lewis <rlewis37@gmail.com>
2017-11-19 01:26:32 -0700

    DD interceptor: hack & strip TF outta it so it works for signin

--- src/app/services/deep.dive.interceptor.ts
+++ src/app/services/deep.dive.interceptor.ts
@@ -25,33 +25,22 @@ export class DeepDiveInterceptor implements HttpInterceptor {
 	 **/
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 		// hand off to the next interceptor
-		return(next.handle(request).map((event: HttpEvent<any>) => {
+
+		const clonedRequest = request.clone({
+			responseType: "text"
+		});
+
+		return(next.handle(clonedRequest).map((event: HttpEvent<any>) => {
 			// if this is an HTTP Response, from Angular...
 			if(event instanceof HttpResponse) {
+
 				// create an event to return (by default, return the same event)
 				let dataEvent = event;
 
-				// if the API is successful...
-				if(event.status === 200) {
+				// extract the data or message from the response body
+				let body = event.body;
 
-					// extract the data or message from the response body
-					let body = event.body;
-					if(body.status === 200) {
-						if(body.data) {
-							// extract data returned from a GET request
-							dataEvent = event.clone({body: body.data});
-						} else if(body.message) {
-							// extract a successful message
-							dataEvent = event.clone({body: {message: body.message, status: 200, type: "alert-success"}});
-						}
-					} else {
-						// extract a failing message when the API fails
-						dataEvent = event.clone({body: {message: body.message, status: body.status, type: "alert-danger"}});
-					}
-				} else {
-					// extract a failing message when the web server fails
-					dataEvent = event.clone({body: {message: event.statusText, status: event.status, type: "alert-danger"}});
-				}
+				dataEvent = dataEvent.clone<any>({body: JSON.parse(dataEvent.body)});
 				return(dataEvent);
 			}
 		}));
