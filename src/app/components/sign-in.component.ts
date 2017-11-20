fd1df619440fb5c41d6526a92cae151b8b64ec02
Rochelle Lewis <rlewis37@gmail.com>
2017-11-20 13:23:15 -0700

    revert sign-in.component to template form std

--- src/app/components/sign-in.component.ts
+++ src/app/components/sign-in.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ViewChild, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
 import {Status} from "../classes/status";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms"
 
 import {SessionService} from "../services/session.service";
 import {SignInService} from "../services/sign-in.service";
@@ -15,27 +14,19 @@ declare const $: any;
 	selector: "sign-in"
 })
 
-export class SignInComponent implements OnInit {
+export class SignInComponent {
 
-	signInForm: FormGroup;
+	@ViewChild("signInForm") signInForm : any;
 	signin: SignIn = new SignIn(null, null);
 	status: Status = null;
 
 	constructor(
 		private sessionService: SessionService,
-		private formBuilder: FormBuilder,
 		private signInService: SignInService,
-		private router: Router) {}
+		private router: Router
+	) {}
 
 	isSignedIn = false;
-
-	ngOnInit() : void {
-		this.signInForm = this.formBuilder.group({
-			profileEmail: ["", [Validators.maxLength(64), Validators.required]],
-			profilePassword: ["", [Validators.maxLength(255), Validators.required]]
-		});
-		this.applyFormChanges();
-	}
 
 	applyFormChanges() : void {
 		this.signInForm.valueChanges.subscribe(values => {
@@ -45,9 +36,9 @@ export class SignInComponent implements OnInit {
 		});
 	}
 
-	/*ngOnChanges() : void {
+	ngOnChanges() : void {
 		this.isSignedIn = this.signInService.isSignedIn;
-	}*/
+	}
 
 	signIn() : void {
 		this.signInService.postSignIn(this.signin)
@@ -59,7 +50,7 @@ export class SignInComponent implements OnInit {
 					this.signInService.isSignedIn = true;
 					this.signInForm.reset();
 					this.router.navigate(["posts"]);
-					//console.log("signin successful");
+					console.log("signin successful");
 					setTimeout(function(){$("#signin-modal").modal('hide');},1000);
 				} else {
 					console.log("failed login");
