8b84983c06aa359a07696ba46239cc88fbfd7aef
Rochelle Lewis <rlewis37@gmail.com>
2017-11-25 01:14:58 -0700

    add signOut() to navbar.component, wire call to logout nav btn

--- src/app/components/navbar.component.ts
+++ src/app/components/navbar.component.ts
@@ -1,14 +1,18 @@
-import {Component, ViewChild, EventEmitter, Output, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import {Router} from "@angular/router";
 import {Status} from "../classes/status";
 
+import {CookieService} from "ng2-cookies";
 import {SignInService} from "../services/sign-in.service";
+
 import {SignUpService} from "../services/sign-up.service";
 import {ProfileService} from "../services/profile.service";
-import {PostService} from "../services/post.service";
 
 import {SignIn} from "../classes/sign-in";
 import {Profile} from "../classes/profile";
+
+//enable jquery $ alias
+declare const $: any;
 
 @Component({
 	templateUrl: "./templates/navbar.html",
@@ -16,5 +20,24 @@ import {Profile} from "../classes/profile";
 })
 
 export class NavbarComponent {
+	status: Status = null;
 
+	constructor(
+		private signInService: SignInService,
+		private cookieService: CookieService,
+		private router: Router
+	){}
+
+	signOut() : void {
+		this.signInService.getSignOut()
+			.subscribe(status => {
+				this.status = status;
+				if(status.status === 200) {
+					this.cookieService.deleteAll();
+					this.router.navigate([""]);
+					location.reload();
+					console.log("goodbye");
+				}
+			});
+	}
 }
