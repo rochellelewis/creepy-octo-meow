b5eb60c21ca971e8cf6bea9fd381268b731d26cb
Rochelle Lewis <rlewis37@gmail.com>
2017-11-24 18:43:03 -0700

    add JWT support in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from "@angular/core";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule} from "@angular/forms";
+import {JwtModule} from "@auth0/angular-jwt";
+
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 
@@ -9,8 +11,20 @@ import {SessionService} from "./services/session.service";
 
 const moduleDeclarations = [AppComponent];
 
+const JwtHelper = JwtModule.forRoot({
+	config: {
+		tokenGetter: () => {
+			return localStorage.getItem("jwt-token");
+		},
+		skipWhenExpired: true,
+		whitelistedDomains: ["localhost:7272", "https:bootcamp-coders.cnm.edu/"],
+		headerName: "X-JWT-TOKEN",
+		authScheme: ""
+	}
+});
+
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing],
+	imports:      [BrowserModule, HttpClientModule, ReactiveFormsModule, routing, JwtHelper],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders]
