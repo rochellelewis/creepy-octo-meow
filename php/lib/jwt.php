0b05e9d6e1b94485267421349fc82b417b951458
Rochelle Lewis <rlewis37@gmail.com>
2017-11-28 15:19:31 -0700

    minor cleanup of jwt.php: exception msgs, docblocks

--- php/lib/jwt.php
+++ php/lib/jwt.php
@@ -11,8 +11,8 @@ use Lcobucci\JWT\{
 };
 
 /**
- * Creates a JSON Web Token to be used on the front end to authenticate users,
- * activate protected routes, and verification of the user logged in.
+ * Creates a JSON Web Token to be used on the front end to authenticate
+ * users, activate protected routes, and verification of the user logged in.
  *
  * This token is visible on the front end and should NEVER contain sensitive info.
  *
@@ -21,12 +21,13 @@ use Lcobucci\JWT\{
  * @param string $value name of the custom object that will be used for validation.
  * @param stdClass $content the actual object that will be used for authentication
  * on the front end
+ * @throws RuntimeException if session is not active.
  **/
 function setJwtAndAuthHeader(string $value, stdClass $content): void {
 
 	//enforce that the session is active
 	if(session_status() !== PHP_SESSION_ACTIVE) {
-		throw(new RuntimeException("session not active"));
+		throw(new RuntimeException("Session not active."));
 	}
 
 	//create the signer object
@@ -55,10 +56,13 @@ function setJwtAndAuthHeader(string $value, stdClass $content): void {
 }
 
 /**
- * Verifies the X-JWT-TOKEN sent by Angular matches the JWT-TOKEN saved in this session.
+ * Verifies the X-JWT-TOKEN sent by Angular matches the JWT-TOKEN saved in
+ * this session.
  *
  * This function uses two custom methods to insure that the JWT-TOKENs match.
- * This function returns nothing, but will throw an exception when something does not match
+ *
+ * This function returns nothing, but will throw an exception when something
+ * does not match.
  **/
 function jwtValidator() {
 	// retrieve the jwt from the header
@@ -69,17 +73,20 @@ function jwtValidator() {
 }
 
 /**
- * Enforces the session contains all necessary information and the JWT in the session
- * matches the JWT sent by angular.
+ * Enforces the session contains all necessary information and the JWT in the
+ * session matches the JWT sent by angular.
  *
  * @return \Lcobucci\JWT\Token the JWT token supplied by angular in the header
+ * @throws InvalidArgumentException if JWT does not exist
+ * @throws InvalidArgumentException if session does not contain a signature
+ * @throws InvalidArgumentException if tokens from session and header do not match
  **/
 function validateJwtHeader () : \Lcobucci\JWT\Token {
 
 	//if the JWT does not exist in the cookie jar throw an exception
 	$headers = array_change_key_case(apache_request_headers(), CASE_UPPER);
 	if(array_key_exists("X-JWT-TOKEN", $headers) === false) {
-		throw new InvalidArgumentException("Invalid Token.", 400);
+		throw new InvalidArgumentException("Invalid Token.", 401);
 	}
 
 	//enforce the session has needed content
@@ -95,7 +102,7 @@ function validateJwtHeader () : \Lcobucci\JWT\Token {
 	if ($_SESSION["JWT-TOKEN"] !== (string)$headerJwt) {
 		$_COOKIE = [];
 		$_SESSION = [];
-		throw (new InvalidArgumentException("Tokens do not match. Please log in again.", 400));
+		throw (new InvalidArgumentException("Tokens do not match. Please log in again.", 401));
 	}
 
 	return $headerJwt;
@@ -105,21 +112,21 @@ function validateJwtHeader () : \Lcobucci\JWT\Token {
  * Enforce that the JWT has not been tampered with and is not expired.
  *
  * @param \Lcobucci\JWT\Token $headerJwt X-JWT-TOKEN sent by Angular
+ * @throws InvalidArgumentException if JWT is invalid
+ * @throws InvalidArgumentException if JWT is not signed by server
  **/
 function verifiedAndValidatedSignature (\Lcobucci\JWT\Token $headerJwt) : void {
 
 	//enforce the JWT is valid
 	$validator = new ValidationData();
 	$validator->setId(session_id());
-
 	if($headerJwt->validate($validator) !== true) {
-		throw (new InvalidArgumentException("Invalid JWT. Not authorized.", 402));
+		throw (new InvalidArgumentException("Invalid JWT. Not authorized.", 401));
 	}
 
 	//verify that the JWT was signed by the server
 	$signer = new Sha512();
-
 	if($headerJwt->verify($signer, $_SESSION["signature"]) !== true) {
-		throw (new InvalidArgumentException("Invalid JWT signature. Not authorized.", 403));
+		throw (new InvalidArgumentException("Invalid JWT signature. Not authorized.", 401));
 	}
 }
