aef816d09fb58e99c55af5d5ea1f14b0e5896e8f
Rochelle Lewis <rlewis37@gmail.com>
2017-12-06 16:28:26 -0700

    create-post.component: get createPost() working w/ UUID off of JWT

--- src/app/components/create-post.component.ts
+++ src/app/components/create-post.component.ts
@@ -1,15 +1,12 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
-import {Observable} from "rxjs";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Status} from "../classes/status";
 
 import {Post} from "../classes/post";
-import {Profile} from "../classes/profile";
 
-import {CookieService} from "ng2-cookies";
+import {JwtHelperService} from "@auth0/angular-jwt";
 import {PostService} from "../services/post.service";
-import {ProfileService} from "../services/profile.service";
 
 //enable jquery $ alias
 declare const $: any;
@@ -23,17 +20,15 @@ export class CreatePostComponent implements OnInit {
 
 	createPostForm: FormGroup;
 	post: Post = new Post(null, null, null, null, null);
-	profile: Profile = new Profile(null, null, null, null, null, null);
+	posts: Post[] = [];
 	status: Status = null;
 
-	cookieJar: any = {};
+	authObj: any = {};
 
 	constructor(
 		private formBuilder: FormBuilder,
-		private cookieService: CookieService,
+		private jwtHelperService: JwtHelperService,
 		private postService: PostService,
-		private profileService: ProfileService,
-		private router: Router
 	){}
 
 	ngOnInit() : void {
@@ -52,33 +47,36 @@ export class CreatePostComponent implements OnInit {
 		});
 	}
 
-	/*getProfile() : void {
-		this.profileService.getProfile()
-			.subscribe(this.profile);
-	}*/
+	reloadPosts() : void {
+		this.postService.getAllPosts()
+			.subscribe(posts => this.posts = posts);
+	}
+
+	getJwtProfileId() : any {
+		this.authObj = this.jwtHelperService.decodeToken(localStorage.getItem('jwt-token'));
+	}
 
 	createPost() : void {
-		//get logged in profile of the author
-		/*this.cookieJar = this.cookieService.getAll();
-		this.profileService.getProfile(this.cookieJar["profileId"])
-			.subscribe(profile => this.profile = profile);*/
+
+		this.getJwtProfileId();
+		let newPostProfileId = this.authObj.auth.profileId;
+		//console.log(newPostProfileId);
 
 		//form new post
-		//let post = new Post(null, this.profile.id, this.createPostForm.value.postContent, null, this.createPostForm.value.postTitle);
-		//post.postProfileId = this.profileService.getProfile(this.profile.profileId);
+		let post = new Post(null, newPostProfileId, this.createPostForm.value.postContent, null, this.createPostForm.value.postTitle);
 
-		/*this.postService.createPost(post)
+		this.postService.createPost(post)
 			.subscribe(status => {
 				this.status = status;
 				if(this.status.status === 200) {
+					this.reloadPosts();
 					this.createPostForm.reset();
-
 					console.log("post created ok");
-					setTimeout(function(){$("#new-post-modal").modal("hide");}, 1000);
+					setTimeout(function(){$("#new-post-modal").modal("hide");}, 3000);
 				} else {
 					console.log("post not created");
 				}
-			});*/
+			});
 	}
 
 }
