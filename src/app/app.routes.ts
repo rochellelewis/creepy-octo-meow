288e73774a132024b36833d9a537da5080cd6501
Rochelle Lewis <rlewis37@gmail.com>
2018-02-28 17:06:36 -0700

    add router param for profileId in app.routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -43,8 +43,8 @@ export const allAppComponents = [
 
 // setup routes
 export const routes: Routes = [
-	//{path: "profile/:id", component: ProfileComponent},
-	{path: "profile", component: ProfileComponent},
+	//{path: "profile", component: ProfileComponent},
+	{path: "profile/:id", component: ProfileComponent},
 	{path: "posts", component: PostsComponent, canActivate: [AuthGuard]},
 	{path: "about", component: AboutComponent},
 	{path: "", component: HomeComponent},
